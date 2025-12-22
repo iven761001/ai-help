07e41badcbfa9c5b923ae844469519edc82f7ee0
iven761001 <127478118+iven761001@users.noreply.github.com>
2025-12-23 00:30:26 +0800

    Update useAvatarController.js

--- app/components/avatar/useAvatarController.js
+++ app/components/avatar/useAvatarController.js
@@ -1,1 +1,21 @@
 
+// app/components/avatar/useAvatarController.js
+"use client";
+
+import { useMemo } from "react";
+
+export default function useAvatarController({ previewYaw = 0, emotion = "idle" }) {
+  // 之後有骨架：這裡會改成 mixer + actions
+  const api = useMemo(() => {
+    return {
+      // 給 Stage/Avatar 用的旋轉參數
+      yaw: previewYaw,
+      emotion,
+      // 之後可以擴充：
+      play: (name) => {},
+      lookAt: (x, y) => {}
+    };
+  }, [previewYaw, emotion]);
+
+  return api;
+}
